6b3cdfe9f255944be5113ac2ac5345b2ecb7142d
mazqt <jesperhipersson@gmail.com>
2021-07-28 02:06:18 +0200

    Made it so that the highscore is updated and displayed properly

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Score from "./Components/Score";
 import "firebase/analytics";
 import "firebase/firestore";
 import CharacterSelect from "./Components/CharacterSelect";
-import uuid from "uuid"
+import uuid from "uuid";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDIZQJvndsGa2X-lEU-G7EtFA3llsyv0-s",
@@ -32,8 +32,10 @@ function App() {
   const [currentID, setCurrentID] = useState(0);
   const [characters, setCharacters] = useState(0);
   const [won, setWon] = useState(false);
-  const [id, setID] = useState("")
-  const [time, setTime] = useState()
+  const [id, setID] = useState("");
+  const [time, setTime] = useState();
+  const [scores, setScores] = useState([]);
+  const [scoreNames, setScoreNames] = useState([]);
   const db = firebase.firestore();
 
   async function loadOptions() {
@@ -56,28 +58,47 @@ function App() {
   useEffect(() => {
     if (characters.length === 0) {
       setWon(true);
-      const end = Date.now()
-      db.collection("times").doc(id).get().then((querySnapshot) => {
-        const start = querySnapshot.data().time
-        setTime((end - start) / 1000)
-      })
+      const end = Date.now();
+      db.collection("times")
+        .doc(id)
+        .get()
+        .then((querySnapshot) => {
+          const start = querySnapshot.data().time;
+          setTime((end - start) / 1000);
+        });
     }
   }, [characters]);
 
   useEffect(() => {
     if (time != null) {
-      db.collection("games").doc(chosenGame).get().then((querySnapshot) => {
-        let highscores = querySnapshot.data().highscores
-        const name = window.prompt("Enter your name so that we can save your score!", "Waldo")
-        highscores.push({[time]: name})
-        highscores.sort((a, b) => {
-          return Object.keys(a)[0] - Object.keys(b)[0]
-        })
-        highscores = highscores.slice(0, 10)
-        db.collection("games").doc(chosenGame).update({highscores: highscores})
-      })
+      db.collection("games")
+        .doc(chosenGame)
+        .get()
+        .then((querySnapshot) => {
+          let highscores = querySnapshot.data().highscores;
+          const name = window.prompt(
+            "Enter your name so that we can save your score!",
+            "Waldo"
+          );
+          highscores.push({ [time]: name });
+          highscores.sort((a, b) => {
+            return Object.keys(a)[0] - Object.keys(b)[0];
+          });
+          highscores = highscores.slice(0, 10);
+          db.collection("games")
+            .doc(chosenGame)
+            .update({ highscores: highscores });
+          let names = [];
+          let scores = [];
+          highscores.forEach((score) => {
+            names.push(Object.values(score)[0]);
+            scores.push(Object.keys(score)[0]);
+          });
+          setScores(scores);
+          setScoreNames(names);
+        });
     }
-  }, [time])
+  }, [time]);
 
   useEffect(() => {
     setLoadingGames(true);
@@ -94,10 +115,10 @@ function App() {
     if (playing) {
       loadGame().then(() => {
         setGameLoaded(true);
-        const start = Date.now()
-        const val = uuid.v4()
-        setID(val)
-        db.collection("times").doc(val).set({time: start})
+        const start = Date.now();
+        const val = uuid.v4();
+        setID(val);
+        db.collection("times").doc(val).set({ time: start });
       });
     }
   }, [playing]);
@@ -130,7 +151,24 @@ function App() {
         </div>
       );
     } else {
-      return <div>You've won!! You did it it {time} seconds</div>;
+      if (time != null) {
+        return (
+          <div>
+            <div>You've won!! You did it it {time} seconds</div>
+            <ol>
+              {scoreNames.map((name, index) => {
+                return (
+                  <li>
+                    {name}: {scores[index]} seconds
+                  </li>
+                );
+              })}
+            </ol>
+          </div>
+        );
+      } else {
+        return <div>You've won!! You did it it {time} seconds</div>;
+      }
     }
   } else {
     if (loadingGames) {
